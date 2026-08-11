20ac1d609d4c0c8714aa26d4babc35541a7f1bc1
Daniel <klovrdaniel@proton.me>
2026-08-11 15:01:31 +0100

    Added directory path safety catch

---
+++ NumericalGBF.py
@@ -0,0 +1,15 @@
+import numpy as np 
+import matplotlib.pyplot as plt
+import os
+import argparse
+from Functions import *
+from matplotlib import rc
+
+plt.rcParams.update({
+    "font.family": "serif",
+    "font.serif": ["Computer Modern Roman"],
+    "mathtext.fontset": "cm",
+    "text.usetex": False
+})
+
+j = complex(0, 1)
